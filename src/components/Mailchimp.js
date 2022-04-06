b3f5a8a515c68dd044f09821045221a3c5cc379b
yillachen <chn.yilla@gmail.com>
2022-04-06 15:08:55 -0400

    fix: netlify not picking up env

--- src/components/Mailchimp.js
+++ src/components/Mailchimp.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Mailchimp from "react-mailchimp-form";
 
+console.log(process.env)
+
 export default function MailchimpForm(props) {
   return (
     <Mailchimp
-      action={process.env.REACT_APP_MAILCHIMP_KEY}
+      action={`https://nightsnackclub.us20.list-manage.com/subscribe/post?u=${process.env.REACT_APP_MAILCHIMP_KEY}&id=${process.env.REACT_APP_MAILCHIMP_ID}`}
       fields={[
         {
           name: "EMAIL",
