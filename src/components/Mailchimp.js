a244836c7fbea1aeb3f022a07a40decd35b4c63a
yillachen <chn.yilla@gmail.com>
2022-04-06 15:01:59 -0400

    fix: mailchimp secret key

--- src/components/Mailchimp.js
+++ src/components/Mailchimp.js
@@ -1,11 +1,10 @@
 import React from "react";
 import Mailchimp from "react-mailchimp-form";
-import { mailchimp_key } from '../secrets'
 
 export default function MailchimpForm(props) {
   return (
     <Mailchimp
-      action={`https://nightsnackclub.us20.list-manage.com/subscribe/post?u=${process.env.MAILCHIMP_U}&id=${process.env.MAILCHIMP_ID}`}
+      action={process.env.REACT_APP_MAILCHIMP_KEY}
       fields={[
         {
           name: "EMAIL",
